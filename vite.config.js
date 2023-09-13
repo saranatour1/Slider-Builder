7213235e1bd2b5af6292feb27cc342b347102b50
Sara <saranatourcontact@gmail.com>
2023-09-14 01:26:32 +0300

    Removed unnessasry declaration

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
   plugins: [react()],
 
 })
